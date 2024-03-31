81a4bd8ed3d353ad0f542f02edec5e894cc015c8
christinalizx <christinalizx@gmail.com>
2024-03-31 00:15:21 -0400

    A5 Commit till 4.3.4

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import express from "express";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 app.listen(4000);
