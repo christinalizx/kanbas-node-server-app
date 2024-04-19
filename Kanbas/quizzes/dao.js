39a21307db4c769ace4e60f8e11d3d6ca2b3ab84
Daniel Lee <lee.daniel6@northeastern.edu>
2024-04-19 12:08:35 -0400

    fixed cancel button

--- Kanbas/quizzes/dao.js
+++ Kanbas/quizzes/dao.js
@@ -1,9 +1,8 @@
 import model from "./model.js";
 
-
 export const createQuiz = (quiz) => model.create(quiz);
 export const findAllQuizzes = () => model.find();
-export const findAllCourseQuizzes = (courseId) => model.find({courseId: courseId});
+export const findAllCourseQuizzes = (courseId) => model.find({ courseId });
 export const findQuizById = (quizId) => model.findById(quizId);
-export const updateQuiz = (quizId, quiz) =>  model.updateOne({ _id: quizId }, { $set: quiz });
+export const updateQuiz = (quizId, quiz) => model.updateOne({ _id: quizId }, { $set: quiz });
 export const deleteQuiz = (quizId) => model.deleteOne({ _id: quizId });
--- Kanbas/quizzes/schema.js
+++ Kanbas/quizzes/schema.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema({
-    title: { type: String, default: '' },
+    title: { type: String, required: true, default: 'New Quiz' },
     quizType: {
         type: String,
         enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"],
         default: "Graded Quiz",
     },
-    points: {type: Number, default: 0}, 
+    points: { type: Number, default: 0 },
     assignmentGroup: { 
         type: String, 
-        enum: ['Quizzes', 'Exams', 'Assignments', 'Project'], 
+        enum: ['Quizzes', 'Exams', 'Assignments', 'Projects'], 
         default: 'Quizzes' 
     },
     shuffleAnswers: { type: Boolean, default: true },
@@ -24,11 +24,10 @@ const quizSchema = new mongoose.Schema({
     dueDate: { type: Date },
     availableDate: { type: Date },
     untilDate: { type: Date },
-    courseId: { type: String },
+    courseId: { type: String, required: true },
     published: { type: Boolean, default: false },
-},
-{ 
+}, {
     collection: "quizzes"
 });
 
-export default quizSchema; 
+export default quizSchema;
